46e3cd237ea1c247359d76edf9ba70a2a72144b6
LAPTOP-LBHE4FM6\Administrator <noriks@nate.com>
2023-04-27 23:43:43 +0900

    icon 목록 페이지 생성

--- src/pages/test/IconPage.vue
+++ src/pages/test/IconPage.vue
@@ -1,13 +1,14 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <q-list>
-      <q-item>
-        <q-item-section>
-          <q-icon name="bluetooth" />
+  <div class="q-pa-md" style="max-width: 350px">
+    <q-list bordered separator>
+      <q-item clickable v-ripple v-for="n in 100" :key="n">
+        <q-item-section avatar>
+          <q-avatar color="purple" text-color="white" icon="bluetooth" />
         </q-item-section>
+        <q-item-section>bluetooth</q-item-section>
       </q-item>
     </q-list>
-  </q-page>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
