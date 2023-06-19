aeb859127f190d7e672a66e9fc1d8e3d230cf726
LAPTOP-LBHE4FM6\Administrator <noriks@nate.com>
2023-06-19 23:58:39 +0900

    File

--- src/pages/template/CreateSource.vue
+++ src/pages/template/CreateSource.vue
@@ -25,34 +25,7 @@
       </template>
 
       <template v-slot:after>
-        <div class="q-pa-sm">
-          <q-tabs
-            v-model="tab"
-            dense
-            class="bg-primary text-white"
-            active-color="indigo-10"
-            indicator-color="indigo-10"
-            :align="`justify`"
-          >
-            <q-tab name="user" label="User" />
-            <q-tab name="alarms" label="Alarms" />
-            <q-tab name="movies" label="Movies" />
-          </q-tabs>
-          <q-tab-panels v-model="tab" animated class="shadow-2 rounded-borders">
-            <q-tab-panel name="user">
-              <div class="text-h6">User</div>
-            </q-tab-panel>
-
-            <q-tab-panel name="alarms">
-              <div class="text-h6">Alarms</div>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </q-tab-panel>
-            <q-tab-panel name="movies">
-              <div class="text-h6">Movies</div>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </q-tab-panel>
-          </q-tab-panels>
-        </div>
+        
       </template>
     </q-splitter>
   </q-page>
